a7be44c468c82b75593c4a050a1ae2315c720c7f
Steven Magelowitz <steven.magelowitz@gmail.com>
2014-03-19 14:25:13 -0600

    Created multidimensional game board initialized pacman at new position
    Signed-off-by: Nathanael Burt <nathanael.burt@gmail.com>

--- pacman-kata/lib/pacman.rb
+++ pacman-kata/lib/pacman.rb
@@ -1,11 +1,14 @@
 class Pacman
   attr_accessor :pacman_state, :game_board, :pacman_location
-  STARTING_LOCATION = 5
+  PACMAN_STARTING_WIDTH = 5
+  PACMAN_STARTING_HEIGHT = 7
+  GAME_WIDTH = 10
+  GAME_HEIGHT = 10
   def initialize
     @pacman_state = 'V'
-    @game_board = ['.','.','.','.','.','.','.','.','.','.','.']
-    @game_board[STARTING_LOCATION] = @pacman_state
+    @game_board = []
     @pacman_location = @game_board.index(@pacman_state)
+    @food = '.'
   end
 
   def move(direction)
@@ -19,6 +22,40 @@ class Pacman
         @pacman_location += 1
         self.position('right')
       end
+  end
+
+  def run
+    self.game_state
+    @game_board[PACMAN_STARTING_HEIGHT][PACMAN_STARTING_WIDTH] = @pacman_state
+    @game_board
+  end
+  #
+  #def pacman_state
+  #
+  #end
+  #
+  #def food_state
+  #
+  #end
+  #
+  #def monster_state
+  #
+  #end
+  #
+  #def wall_state
+  #
+  #end
+
+  def game_state
+    @game_board = []
+    GAME_HEIGHT.times do
+      line_array = []
+      GAME_WIDTH.times do
+        line_array << @food
+      end
+      @game_board << line_array
+    end
+    @game_board
   end
 
   def position(direction)
--- pacman-kata/spec/pacman_spec.rb
+++ pacman-kata/spec/pacman_spec.rb
@@ -3,12 +3,14 @@ require 'pacman'
 require_relative('../bin/pacman_cli')
 
 describe 'Pacman CLI' do
-  it 'shows pacman on a grid filled with dots' do
+  it 'shows initial state of pacman on a grid filled with dots' do
     game = Pacman.new
-    expect(game.pacman_state).to eq('V')
+    game.run
+    expect(game.game_board.flatten.include?('V')).to eq true
   end
 
   it 'shows pacman\'s direction based on input' do
+    pending
     game = Pacman.new
     game.move("left")
 
@@ -19,16 +21,32 @@ describe 'Pacman CLI' do
   end
 
   it 'should return pacman\'s index in the game board' do
+    pending
     game = Pacman.new
     expect(game.pacman_location).to eq 5
   end
 
   it 'shows pacman moving to different positions in the array' do
+    pending
     game = Pacman.new
     game.move("left")
     expect(game.pacman_location).to eq 4
 
     game.move("right")
     expect(game.pacman_location).to eq 5
+  end
+
+  it 'initializes with a 10 by 10 game board populated with pacman and food' do
+    game = Pacman.new
+    expect(game.run).to eq [['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','V','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],
+                            ['.','.','.','.','.','.','.','.','.','.'],]
   end
 end
